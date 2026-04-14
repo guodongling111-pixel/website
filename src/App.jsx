7442609b8a36ec550bf7f81773cd3041ac7f731f
郭冬玲 <guodongling111@gmail.com>
2026-04-14 16:03:07 +0800

    feat: add personal website UI and generation prompt integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import BackgroundEffect from './components/BackgroundEffect';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import ProjectDetail from './pages/ProjectDetail';
@@ -9,6 +10,7 @@ import './App.css';
 function App() {
   return (
     <BrowserRouter>
+      <BackgroundEffect />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
